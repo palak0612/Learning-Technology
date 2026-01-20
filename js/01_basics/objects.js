e24815d1ed1cfd68987e84824f600899b553a293
Palak <palak0608bhatia@gmail.com>
2026-01-20 22:36:25 +0530

    Updates

---
+++ js/01_basics/objects.js
@@ -0,0 +1,12 @@
+//singleton - if you make a object
+// if we make object thorough literals then singleton is not made
+// object literals
+const JsUser = {
+  name : "Palak",
+  age : 18,
+  location: "gurugram",
+  email: "abs@gmail.com",
+  isloggedIn: false
+}
+
+console.log(JsUser.email);
