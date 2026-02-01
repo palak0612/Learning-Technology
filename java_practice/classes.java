3ba699313f5cec575ecd2460471f0dc7e39800d4
Palak <173133500+palak0612@users.noreply.github.com>
2026-02-01 21:41:56 +0530

    Learning arrays

--- java_practice/classes.java
+++ java_practice/classes.java
@@ -7,14 +7,20 @@ class Calculator {
     int r = n1 + n2;
     return r;
   }
+
+  public String getmeAPen(){
+    return "Pen";
+  }
 }
 
 public class classes {
   public static void main(String a[]) {
     int num1 = 3;
     int num2 = 5;
-    Calculator Calc = new Calculator();
+    Calculator Calc = new Calculator(); // creating object for the calculator class
     int result = Calc.add(num1, num2);
     System.out.print(result);
+    String Str = Calc.getmeAPen();
+    System.out.print(Str);
   }
 }
