7da0d9260233526d37d4a3623c8d4a9219fd7b42
Palak <173133500+palak0612@users.noreply.github.com>
2026-02-05 13:03:51 +0530

    learning linear search

---
+++ java_practice/Linear_search.java
@@ -0,0 +1,19 @@
+package java_practice;
+public class Linear_search {
+    public static void main(String[] args){
+      int[] nums = {23,13,15,21,76,78,45};
+      int target = 76;
+      int ans = linearSearch(nums, target);
+      System.out.println(ans);
+    }
+
+
+static int linearSearch(int[] arr, int target){
+         if(arr.length == 0) return -1;
+        for(int index = 0; index < arr.length; index++){
+          int element = arr[index];
+          if(element == target) return index;
+        }
+        return -1;
+}
+}
--- java_practice/oops.java
+++ java_practice/oops.java
@@ -1,8 +1,27 @@
 package java_practice;
 
 class Human{
-   
+   private int age;
+   private String name;
+   public int getAge(){ // getter
+    return age;
+   } 
+   public void setAge(int a){ // setter 
+    age = a;
+   }
+   public void setName(String n){
+    name = n;
+   }
+   public String getName(){
+    return name;
+   }
 }
+
 public class oops {
-   
+   public static void main(String[] args) {
+    Human obj = new Human();
+    obj.setAge(30);
+    obj.setName("palak");
+    System.out.println(obj.getName() + " " + obj.getAge());
+   }
 }
